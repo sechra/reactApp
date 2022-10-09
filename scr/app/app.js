3f2c0d13c20ccff6afad0523526d6d20e8841b03
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2022-10-09 08:16:03 -0700

    create app.js

---
+++ scr/app/app.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from '../header/Header'
+import Main from '../main/Main'
+import Footer from '../footer/Footer'
+import React from 'react';
+
+class App extends React.Component {
+
+render() {
+  return (
+    <div className="App">
+      
+<Header />
+<Main />
+<Footer />
+
+      
+      
+    </div>
+  );
+}
+}
+
+export default App;
